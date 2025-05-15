fc994887a961d0f2fe495c1c02959597c222c262
ayaankamal26 <161169872+ayaankamal26@users.noreply.github.com>
2025-05-15 14:27:41 -0400

    Added inflation by importing cpi data and calculating yoy inflation

--- InflationForecast.py
+++ InflationForecast.py
@@ -8,3 +8,12 @@ Created on Thu May 15 13:56:06 2025
 
 import numpy as np
 import pandas as pd
+
+#read in CPI data for inflation
+cpi = pd.read_csv("~/Downloads/CPIAUCSL.csv")
+yoy = []
+dates = list(cpi.iloc[12:,0])
+for i in range(12, len(cpi)):
+    yearInf = ((cpi.iloc[i, 1]-cpi.iloc[i-12, 1])/cpi.iloc[i-12, 1]) * 100
+    yoy.append(yearInf)
+df = pd.DataFrame(zip(dates, yoy), columns = ["Date", "YoY Inflation"])
